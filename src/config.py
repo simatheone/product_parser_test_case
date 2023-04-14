825d521d3b0ef67848ba3e7f9922bb8f7098b043
simatheone <samone07@mail.ru>
2023-04-14 16:39:07 +0300

    feat: add project settings

---
+++ src/config.py
@@ -0,0 +1,52 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class AppSettings(BaseSettings):
+    """Application settings."""
+
+    DB_ENGINE: str
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+
+    class Config:
+        env_file = '.env'
+
+    @property
+    def database_url(self) -> str:
+        """Url for connection to database.
+
+        Returns:
+        - str: The URL used to connect to the database based on the settings
+        stored in the `AppSettings` instance.
+
+        Raises:
+        - None.
+        """
+
+        return (
+            f'{self.DB_ENGINE}://'
+            f'{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}'
+            f'@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        )
+
+
+@lru_cache
+def get_settings():
+    """Retrieves the application's settings and returns an instance of the
+    `AppSettings` class containing these settings.
+
+    Returns:
+    - AppSettings: An instance of `AppSettings` with the application's settings.
+
+    Raises:
+    - None.
+    """
+    return AppSettings()  # type: ignore
+
+
+settings = get_settings()
