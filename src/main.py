09a7d7611f0b9e4c02d70fdc711ed079f3507e2e
simatheone <samone07@mail.ru>
2023-04-15 20:49:13 +0300

    feat: add CORSMiddleware

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from src.config import settings
 from src.router import router as product_router
 
 app = FastAPI()
 
 app.include_router(product_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOW_ORIGINS,
+    allow_methods=(
+        'HEAD',
+        'OPTIONS',
+        'GET',
+        'POST',
+        'DELETE',
+        'PATCH',
+    ),
+    allow_headers=('*'),
+)
