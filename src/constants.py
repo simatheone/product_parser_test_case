f76d9fc9a05495058f3f20d89444447b53a07461
simatheone <samone07@mail.ru>
2023-04-16 13:30:37 +0300

    feat: add additional responses for product endpoints

--- src/constants.py
+++ src/constants.py
@@ -1,16 +1,3 @@
-class ErrorCodes:
-    NOT_FOUND = 'Not Found'
-    BAD_REQUEST = 'Bad Request'
-    SERVER_ERROR = 'Server Error'
-    PRODUCT_NOT_FOUND = (
-        'Unable to find a product with the provided ID. '
-        'Check the ID of the product you are entering.'
-    )
-    PRODUCT_DOES_NOT_EXIST = 'Product does not exist.'
-    PRODUCT_ALREADY_EXISTS = 'Product with this id already exists.'
-    SMTH_WENT_WRONG = 'Something went wrong on requesting data from the website.'
-
-
 URL_LINK = 'https://card.wb.ru/cards/detail?curr=rub&dest=-1257786&spp=0&nm={nm_id}'
 
 RESPONSE_KEY_MAPPING = {
@@ -26,3 +13,60 @@ RESPONSE_KEY_MAPPING = {
     'rating': 'rating',
     'feedbacks': 'feedbacks',
 }
+
+
+class ErrorCodes:
+    """Constants for exception messages."""
+
+    NOT_FOUND = 'Not Found'
+    BAD_REQUEST = 'Bad Request'
+    SERVER_ERROR = 'Server Error'
+    PRODUCT_NOT_FOUND = (
+        'Unable to find a product with the provided ID. '
+        'Check the ID of the product you are entering.'
+    )
+    PRODUCT_DOES_NOT_EXIST = 'Product does not exist.'
+    PRODUCT_ALREADY_EXISTS = 'Product with this id already exists.'
+    SMTH_WENT_WRONG = 'Something went wrong on requesting data from the website.'
+
+
+class AdditionalResponses:
+    """Additional error repsonses for Product endpoints."""
+
+    PRODUCT_GET_DELETE = {
+        404: {
+            'content': {
+                'application/json': {
+                    'example': {'detail': ErrorCodes.PRODUCT_DOES_NOT_EXIST}
+                },
+            },
+            'description': 'When a product does not exist in database.',
+        }
+    }
+    PRODUCT_CREATE = {
+        400: {
+            'content': {
+                'application/json': {
+                    'example': {'detail': ErrorCodes.PRODUCT_ALREADY_EXISTS}
+                },
+            },
+            'description': 'When a product already exists in database.',
+        },
+        404: {
+            'content': {
+                'application/json': {
+                    'example': {'detail': ErrorCodes.PRODUCT_NOT_FOUND}
+                },
+            },
+            'description': 'When you entered invalid product id.',
+        },
+        500: {
+            'content': {
+                'application/json': {'example': {'detail': ErrorCodes.SMTH_WENT_WRONG}},
+            },
+            'description': (
+                'Due to some reasons it is impossible to '
+                'make a proper request to the Wildberries website.'
+            ),
+        },
+    }
--- src/router.py
+++ src/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.constants import AdditionalResponses
 from src.dependencies import (
     get_async_session,
     validate_product_exists,
@@ -21,11 +22,14 @@ router = APIRouter(
 @router.get('/all', response_model=list[ProductResponse])
 async def get_all_products(session: AsyncSession = Depends(get_async_session)):
     """Endpoint to retrieve all products from the database."""
-
     return await product_service.get_product_multi(session)
 
 
-@router.get('/{product_id}', response_model=ProductResponse)
+@router.get(
+    '/{product_id}',
+    response_model=ProductResponse,
+    responses={**AdditionalResponses.PRODUCT_GET_DELETE},
+)
 async def get_single_product(
     product_id: int = Depends(validate_product_exists),
     session: AsyncSession = Depends(get_async_session),
@@ -34,11 +38,15 @@ async def get_single_product(
 
     - **product_id** (int): The ID of the product to be retrieved (path parameter).
     """
-
     return await product_service.get_product(product_id, session)
 
 
-@router.post('/', response_model=ProductResponse, status_code=status.HTTP_201_CREATED)
+@router.post(
+    '/',
+    response_model=ProductResponse,
+    status_code=status.HTTP_201_CREATED,
+    responses={**AdditionalResponses.PRODUCT_CREATE},
+)
 async def create_new_product(
     product_id: ProductRequest = Depends(validate_unique_product),
     session: AsyncSession = Depends(get_async_session),
@@ -50,12 +58,15 @@ async def create_new_product(
     - **product_id** (int): The ID of the product to be fetched and created
       (query parameter).
     """
-
     colors, product_info = get_product_data_from_website(product_id.nm_id)
     return await product_service.create_product(product_info, colors, session)
 
 
-@router.delete('/{product_id}', status_code=status.HTTP_204_NO_CONTENT)
+@router.delete(
+    '/{product_id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+    responses={**AdditionalResponses.PRODUCT_GET_DELETE},
+)
 async def delete_product(
     product_id: int = Depends(validate_product_exists),
     session: AsyncSession = Depends(get_async_session),
@@ -64,6 +75,5 @@ async def delete_product(
 
     - **product_id** (int): The ID of the product to be deleted (path parameter).
     """
-
     await product_service.remove_product(product_id, session)
     return
